53566d83f55c9bb8ce334363579a4198c447daa9
Sam Mohr <sam@mohr.codes>
2020-10-12 00:28:23 -0400

    avoid requiring user input in setup scripts

--- .setup/install_repos.sh
+++ .setup/install_repos.sh
@@ -5,10 +5,10 @@ curl -sSL https://dist.crystal-lang.org/apt/setup.sh | sudo bash
 
 # gh repo
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-sudo apt-add-repository -u https://cli.github.com/packages
+sudo apt-add-repository -y https://cli.github.com/packages
 
 # fonts repo
 sudo add-apt-repository -y universe
 
 # lazygit repo
-sudo add-apt-repository ppa:lazygit-team/release
+sudo add-apt-repository -y ppa:lazygit-team/release
