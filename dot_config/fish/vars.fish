2e2a011fb1581ec96249a7ac08bd021ceeb70b6a
Sam Mohr <sam.mohr@protonmail.com>
2022-09-18 21:56:44 -0700

    Add Go Binaries Back to $PATH

--- dot_config/fish/vars.fish
+++ dot_config/fish/vars.fish
@@ -4,7 +4,7 @@ set -xg BAT_THEME ansi
 set -xg GOPATH ~/.go
 set -xg PATH ~/.cargo/bin ~/.local/bin \
     ~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin \
-    ~/.gobrew/bin ~/.gobrew/current/bin \
+    ~/.go/bin ~/.gobrew/bin ~/.gobrew/current/bin \
     ~/.fly/bin ~/.bun/bin $PATH
 
 set -xg LS_COLORS (cat ~/.config/fish/ls-colors.txt)
