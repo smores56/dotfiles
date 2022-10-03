eda3dcd728acffd94fde8b1c1440767025c854ec
Sam Mohr <sam.mohr@protonmail.com>
2022-10-02 21:21:40 -0700

    Switch to Tide Prompt

--- dot_config/fish/fundle.fish
+++ dot_config/fish/fundle.fish
@@ -2,7 +2,7 @@ if not functions -q fundle
     eval (curl -sfL https://git.io/fundle-install)
 end
 
-fundle plugin 'jorgebucaran/hydro'
+fundle plugin 'IlanCosman/tide@v5'
 fundle plugin 'jorgebucaran/autopair.fish'
 fundle plugin 'franciscolourenco/done'
 fundle plugin 'PatrickF1/fzf.fish'
