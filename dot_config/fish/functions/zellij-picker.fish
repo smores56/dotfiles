926b8d5818db2724210088077d781e7206e8236a
Sam Mohr <sam.mohr@protonmail.com>
2022-10-09 11:57:59 -0700

    Error Out When Failing to Pick Zellij Session

--- dot_config/fish/functions/zellij-picker.fish
+++ dot_config/fish/functions/zellij-picker.fish
@@ -2,7 +2,9 @@ function zellij-picker --description "Pick a zellij session"
     if test (count $argv) -gt 0
         zellij attach $argv[1] 2>/dev/null; or zellij -s $argv[1]
     else if zellij list-sessions 2>/dev/null
-        zellij attach (zellij list-sessions | gum choose)
+        set session (zellij list-sessions | gum choose);
+        or return (error "You must pick a session!")
+        zellij attach "$session"
     else
         zellij -s main
     end
