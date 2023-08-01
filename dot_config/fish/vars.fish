bc0190bf4f2e5a80cd459a458f0490ae7ac7c47a
Sam Mohr <sam@sammohr.dev>
2023-07-31 21:36:20 -0700

    More Changes

--- dot_config/fish/vars.fish
+++ dot_config/fish/vars.fish
@@ -5,5 +5,9 @@ set -xg PATH ~/.cargo/bin ~/.rustup/toolchains/*/bin ~/.local/bin \
 set -xg BAT_THEME ansi
 set -xg LS_COLORS (cat ~/.config/fish/ls-colors.txt)
 set -xg GUM_CHOOSE_CURSOR_FOREGROUND 3
-set -xg EDITOR (which helix 1>/dev/null 2>/dev/null;
-                and echo helix; or echo hx)
+
+if which helix 1>/dev/null 2>/dev/null
+    set -xg EDITOR helix
+else
+    set -xg EDITOR hx
+end
